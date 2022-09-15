31ce1425e207899835bbeae6ad8d318b2fd2c938
corbs <corbtastik@gmail.com>
2022-09-15 17:14:20 -0500

    first commit

---
+++ about.md
@@ -0,0 +1,48 @@
+---
+layout: page
+title: about
+---
+
+Howdy amigo! Welcome to my abode where I occasionally blog about tech and post pics.
+
+{%
+  include image/image.html
+  classes="is-circle-128"
+  src="site/me.png"
+  description="Sunlight on my cranium makes me happy."
+%}
+
+## work
+
+I'm a developer by trade with roots in C and Java. I once read something like - _"write code every day"_ and although I haven't, I like to try.
+
+* [__Red Hat__](https://www.redhat.com): Current gig as an Application Architect, Container, and K8s nerd.
+* [__MongoDB__](https://www.mongodb.com): Solutions Architect for my favorite database.
+* [__Pivotal Software__](https://en.wikipedia.org/wiki/Pivotal_Software): Platform Architect, Pivotal Cloud Foundry, Spring Boot, and Redis geek.
+* [__PDX__](https://www.linkedin.com/company/pdx/): Software Architect and Java developer for a Pharmacy applications.
+* [__Raytheon__](https://raytheon.com): Software Developer for remote sensing platforms, lots of math, Java and C/C++.
+* __Martin Household__: "Home Eco" on steroids, laborer, day-dreamer.
+
+
+## schooling
+
+Just when I think I "know" something, there's a "wrinkle", and I don't. Schooling has a start and end date but learning is a lifelong pursuit.
+
+* [__Southern Methodist University__](https://www.smu.edu): Graduate work for Computer Engineering.
+* [__University of Texas at Arlington__](https://www.uta.edu): Undergrad degree in Computer Science.
+* [__Tarrant County College__](https://www.tccd.edu): Associates degree in Microelectronics.
+
+## faves
+
+> When the dog bites, when the bee stings, when I'm feeling sad, I simply remember my favorite things, and then I don't feel so bad.
+
+### Apple Mac Pros
+
+The [Apple Mac Pro](https://www.apple.com/mac-pro/) has been a "hobby" machine of mine for a while. The [2010, Mac Pro 5,1](https://everymac.com/systems/apple/mac_pro/specs/mac-pro-twelve-core-2.93-mid-2010-westmere-specs.html) was my first dream machine. Stuffed with 128 GB memory, 2 Intel Xeon chips (12 cores total), 4 TB of storage, and a 30-inch Cinema Display, it's still useful in 2022. Used as a vSphere homelab and at one point this machine ran 50 virtual machine and a deployment of Pivotal Cloud Foundry and Enterprise MongoDB...fun times!
+
+### Sipping Joe
+
+### Exploring
+
+### Fur Babies
+
---
+++ assets/js/yolo.js
@@ -0,0 +1,335 @@
+// ============================================================================
+// Yolo Javascript functions
+// https://github.com/corbtastik/yolo
+// MIT License
+// ============================================================================
+(function (window){
+    // ------------------------------------------------------------------------
+    // Hack wrapper around console.log for debugging, off by default.
+    // ------------------------------------------------------------------------
+    class Console {
+        static println(text) {
+            if(Console.enabled()) {
+                console.log(text);
+            }
+        }
+        static log(text) {
+            Console.println("YOLO LOG " + new Date().toUTCString() + ": " + text);
+        }
+        static error(text) {
+            Console.println("YOLO ERR " + new Date().toUTCString() + ": " + text);
+        }
+        static enabled() {
+            return true;
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    // ImageGrid class for image-grid logic
+    // ------------------------------------------------------------------------
+    class ImageGrid {
+        constructor(name) {
+            this.name = name;
+        }
+
+        static create(name) {
+            Console.log("ImageGrid.create for name=" + name);
+            return new ImageGrid(name);
+        }
+
+        static dom(name) {
+            return document.getElementById("ig-" + name);
+        }
+
+        static domTarget(name) {
+            return document.getElementById("ig-target-" + name);
+        }
+    }
+
+    ImageGrid.prototype.name = function() {
+        return this.name;
+    }
+
+    ImageGrid.prototype.openImage = function(imageId) {
+        Console.log("ImageGrid.openImage: " + this.name);
+        // Hide the grid
+        ImageGrid.dom(this.name).style.display = "none";
+        // Show the ig-target, hide all images, show the one passed in.
+        const imageTarget = ImageGrid.domTarget(this.name);
+        const images = imageTarget.getElementsByTagName('img');
+        for(let i = 0; i < images.length; i++) {
+            const caption = document.getElementById(this.name + "-caption-" + i);
+            if(images[i].id === imageId) {
+                images[i].style.display = "block";
+                caption.style.display = "block";
+            } else {
+                images[i].style.display = "none";
+                caption.style.display = "none";
+            }
+        }
+        imageTarget.classList.toggle("no-display");
+        imageTarget.style.display = "block";
+    };
+
+    ImageGrid.prototype.closeImage = function() {
+        Console.log("ImageGrid.closeImage: " + this.name);
+        // Hide images and ig-target
+        const imageTarget = ImageGrid.domTarget(this.name);
+        const images = imageTarget.getElementsByTagName('img');
+        for(let i = 0; i < images.length; i++) {
+            const caption = document.getElementById(this.name + "-caption-" + i);
+            caption.style.display = "none";
+            images[i].style.display = "none";
+        }
+        imageTarget.style.display = "none";
+        imageTarget.classList.toggle("no-display");
+        // Show the grid
+        ImageGrid.dom(this.name).style.display = "block";
+    };
+    // ------------------------------------------------------------------------
+    // Lightbox class for lightbox logic
+    // ------------------------------------------------------------------------
+    class Lightbox {
+        constructor(name) {
+            this.name = name;
+            this.isOpen = false;
+            this.slideIndex = 0;
+            this.showSlides(this.slideIndex);
+        }
+
+        static create(name) {
+            Console.log("Lightbox.create for name=" + name);
+            return new Lightbox(name);
+        }
+
+        static dom(name) {
+            return document.getElementById("lb-modal-" + name);
+        }
+
+        static domSlides(name) {
+            const modal = Lightbox.dom(name);
+            return modal.getElementsByClassName("lb-slides");
+        }
+
+        static domCaption(name) {
+            return document.getElementById("lb-caption-" + name);
+        }
+    }
+
+    Lightbox.prototype.name = function() {
+        return this.name;
+    };
+
+    Lightbox.prototype.openModal = function() {
+        Console.log("Lightbox.openModal: " + this.name);
+        Lightbox.dom(this.name).style.display = "block";
+        this.isOpen = true;
+    };
+
+    Lightbox.prototype.closeModal = function() {
+        Console.log("Lightbox.closeModal: " + this.name);
+        Lightbox.dom(this.name).style.display = "none";
+        this.isOpen = false;
+    };
+
+    Lightbox.prototype.plusSlides = function(n) {
+        Console.log("Lightbox.plusSlides: name=" + this.name + " slide=" + n);
+        this.showSlides(this.slideIndex += n);
+    };
+
+    Lightbox.prototype.currentSlide = function(n) {
+        Console.log("Lightbox.currentSlide: name=" + this.name + " slide=" + n);
+        this.showSlides(this.slideIndex = n);
+    };
+
+    Lightbox.prototype.showSlides = function(n) {
+        let i;
+        const slides = Lightbox.domSlides(this.name);
+        const caption = Lightbox.domCaption(this.name);
+        if(n >= slides.length) {
+            this.slideIndex = 0;
+        }
+        if(n < 0) {
+            this.slideIndex = slides.length - 1;
+        }
+        for(i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        Console.log("Lightbox.showSlides: name=" + this.name + " slide=" + this.slideIndex);
+        slides[this.slideIndex].style.display = "block";
+        caption.innerHTML = slides[this.slideIndex].children[0].alt;
+    };
+
+    // ------------------------------------------------------------------------
+    // Yolo class for the site logic
+    // ------------------------------------------------------------------------
+    class Yolo {
+        constructor() {
+            this.lightboxes = new Map();
+            this.imageGrids = new Map();
+        }
+        static create() {
+            const yolo = new Yolo();
+            // Initialize image scaling
+            yolo.scaleImages();
+            // Initialize copy code snippets
+            yolo.copySnippet();
+            // Initialize dark / light theme toggle
+            yolo.themeToggle();
+            // Initialize sidebar
+            yolo.initSidebar();
+            return yolo;
+        }
+        static themes() {
+            return ["corbs", "cloudy"];
+        }
+    }
+
+    Yolo.prototype.initSidebar = function() {
+        const sidebar = document.getElementById('sidebar');
+        if(sidebar !== null) {
+            Console.log("Sidebar is enabled, adding event listener.");
+            sidebar.addEventListener('click', function() {
+                const sidebarWidth = sidebar.style.width;
+                if(sidebarWidth === "50%") {
+                    sidebar.style.width = "1rem";
+                } else {
+                    sidebar.style.width = "50%";
+                }
+            }, false);
+        } else {
+            Console.log("Sidebar is disabled.");
+        }
+    };
+
+    Yolo.prototype.scaleImages = function() {
+        const images = document.querySelectorAll('.image');
+        images.forEach((image, index) => {
+            image.addEventListener("click", () => {
+                Console.log("Yolo.scaleImage to max: " + image.src);
+                image.classList.toggle("max");
+            });
+        });
+    };
+
+    Yolo.prototype.copySnippet = function() {
+        const blocks = document.querySelectorAll(
+            '.code-header + .highlighter-rouge');
+        const elements = document.querySelectorAll(
+            '.code-header');
+        elements.forEach((element, index) => {
+            const code = blocks[index].innerText;
+            element.addEventListener('click', () => {
+                window.navigator.clipboard.writeText(code).then(r => {
+                    Console.log("Yolo.copySnippet to clipboard.");
+                });
+                element.classList.add('copied');
+                const infoText = element.innerHTML.trim();
+                element.innerHTML = element.innerHTML + " copied!"
+                setTimeout(() => {
+                    element.innerHTML = infoText;
+                    element.classList.remove('copied');
+                }, 2000);
+            });
+        });
+    };
+
+    Yolo.prototype.lightbox = function(name) {
+        // Return lightbox instance if it exists
+        if(this.lightboxes.has(name)) {
+            Console.log("Yolo.lightbox returning existing Lightbox: name=" + name);
+            return this.lightboxes.get(name);
+        }
+        // Protection against lightbox craziness
+        if(this.lightboxes.size > 20) {
+            Console.log("Yolo.lightbox max instances " + this.lightboxes.size + " reached.");
+            throw "Error: Yolo lightbox limit, perhaps start another single page site?";
+        }
+        // Create new lightbox instance, setup event listener for prev, next links
+        const lightbox = Lightbox.create(name);
+        Console.log("Yolo.lightbox, created new instance named: " + name);
+        window.document.addEventListener("keydown", event => {
+            if(lightbox.isOpen) {
+                if (event.key === "ArrowLeft" || (event.key === "<" && event.shiftKey)) {
+                    Console.log("Yolo.lightbox keydown ArrowLeft clicked.");
+                    lightbox.plusSlides(-1);
+                } else if (event.key === "ArrowRight" || (event.key === ">" && event.shiftKey)) {
+                    Console.log("Yolo.lightbox keydown ArrowRight clicked.");
+                    lightbox.plusSlides(1);
+                }
+            }
+        });
+        this.lightboxes.set(name, lightbox);
+        return this.lightboxes.get(name);
+    };
+
+    Yolo.prototype.imageGrid = function(name) {
+        // Return image-grid instance if it exists
+        if(this.imageGrids.has(name)) {
+            return this.imageGrids.get(name);
+        }
+        // Protect against image-grid craziness
+        if(this.imageGrids.size > 20) {
+            Console.error("Yolo.imageGrid max instances " + this.imageGrids.size + " reached.");
+            throw "Error: Yolo imageGrid limit, perhaps start another single page site?";
+        }
+        // Create new image-grid instance
+        const imageGrid = ImageGrid.create(name);
+        Console.log("Yolo.imageGrid, created new instance named: " + name);
+        this.imageGrids.set(name, imageGrid);
+        return this.imageGrids.get(name);
+    };
+
+    Yolo.prototype.themeToggle = function() {
+        const yoloSite = document.getElementById("yolo-site");
+        const themeDot = document.getElementById("theme-dot");
+
+        if(yoloSite === null || themeDot === null) {
+            Console.error("Yolo Site and/or Theme Dot is null, perhaps verify <body id=\"yolo-site\">");
+            return;
+        }
+
+        themeDot.addEventListener("click", function() {
+            const activeTheme = localStorage.getItem("active-theme");
+            if(activeTheme === "dark-theme") {
+                Console.log("Switching active-theme to light-theme");
+                yoloSite.classList.remove("dark-theme");
+                localStorage.setItem("active-theme", "light-theme");
+                yoloSite.classList.add("light-theme");
+            } else if(activeTheme === "light-theme") {
+                Console.log("Switching active-theme to dark-theme");
+                yoloSite.classList.remove("light-theme");
+                localStorage.setItem("active-theme", "dark-theme");
+                yoloSite.classList.add("dark-theme");
+            } else if(activeTheme == null) {
+                Console.log("The active-theme is null, setting to light-theme.");
+                localStorage.setItem("active-theme", "light-theme");
+                yoloSite.classList.add("light-theme");
+            } else {
+                Console.error("The active-theme is set to an invalid theme value: "
+                    + activeTheme + ", setting to light-theme.");
+                localStorage.setItem("active-theme", "light-theme");
+                yoloSite.classList.add("light-theme");
+            }
+        });
+
+        window.addEventListener("load", function() {
+            const activeTheme = localStorage.getItem("active-theme");
+            Console.log("Window load, active-theme: " + activeTheme);
+            if(activeTheme === "dark-theme") {
+                localStorage.setItem("active-theme", "dark-theme");
+                yoloSite.classList.add("dark-theme");
+            } else {
+                localStorage.setItem("active-theme", "light-theme");
+                yoloSite.classList.add("light-theme");
+            }
+            Console.log("Window load, transitioning to opacity 1");
+            yoloSite.style.opacity = "1";
+        });
+    }
+    // ------------------------------------------------------------------------
+    // Expose Yolo on the window object.
+    //   Call from HTML: onclick="Yolo.lightbox("lb-images").openModal();"
+    // ------------------------------------------------------------------------
+    window.Yolo = Yolo.create();
+})(window);
