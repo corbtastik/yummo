a04d3bb5c2dd18ce2d73c5feea8e4d05aa5c666b
corbs <corbtastik@gmail.com>
2022-10-07 17:25:48 -0500

    WIP hacking custom js sample to do some color magic on hover

--- assets/ext/js/custom.js
+++ assets/ext/js/custom.js
@@ -1,38 +1,114 @@
-function storageInstance() {
-    let storage;
-    try {
-        storage = window['localStorage'];
-        const x = '__storage_test__';
-        storage.setItem(x, x);
-        storage.removeItem(x);
-        return storage;
-    } catch (e) {
-        return e instanceof DOMException && (
-            // everything except Firefox
-            e.code === 22 ||
-            // Firefox
-            e.code === 1014 ||
-            // test name field too, because code might not be present
-            // everything except Firefox
-            e.name === 'QuotaExceededError' ||
-            // Firefox
-            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-        // acknowledge QuotaExceededError only if there's something already stored
-        (storage && storage.length !== 0);
+/**
+ * rainbowify links
+ **/
+(function (window, document) {
+    // execute javascript in strict mode
+    'use strict';
+    let hoveredLink;
+
+    // ---------- sparkle and shine ---------- //
+
+    function SparkleShineLink(element) {
+        this.element = element;
+        this.chars = element.querySelectorAll('.char');
+        this.charsLength = this.chars.length;
+        // return if there are no chars
+        if (!this.charsLength) {
+            return;
+        }
+        this.endIndex = 0;
+        this.startIndex = 0;
+        this.hueIndex = Math.floor(Math.random() * 360);
+        this.colors = [];
+        this.isHovered = true;
+        this.isSparkling = true;
+        this.sparkle();
+        // only one hoverLink at a time
+        hoveredLink = element;
+        // detect when the hover is over
+        document.addEventListener('mouseover', this, false);
     }
-}
 
-function testStorage() {
-    let storage = storageInstance();
-    if(storage.getItem("message") !== null) {
-        console.log("Message already saved to localStorage: " + storage.getItem("message"));
-    } else {
-        storage.setItem("message", "Howdy from custom.js at " + new Date().toUTCString());
-        console.log(storage.getItem("message"));
+    // ---------- event handlers ---------- //
+
+    SparkleShineLink.prototype.handleEvent = function (event) {
+        let method = event.type + 'Handler';
+        if (this[method]) {
+            this[method](event);
+        }
+    };
+
+    SparkleShineLink.prototype.mouseoverHandler = function (event) {
+        if (this.element.contains(event.target)) {
+            return;
+        }
+        this.isHovered = false;
+        hoveredLink = null;
+        document.removeEventListener('mouseover', this, false);
+    };
+
+    // ---------- actions ---------- //
+
+    SparkleShineLink.prototype.sparkle = function () {
+        this.endIndex = Math.min(this.endIndex + 1, this.charsLength);
+
+        // add colors
+        let hue = (this.hueIndex * 10) % 360;
+        let nextColor = this.isHovered ? 'hsl(' + hue + ', 100%, 50% )' : 'white';
+        // move next color to the front
+        this.colors.unshift(nextColor);
+        // change colors of characters
+        for (let i = this.startIndex; i < this.endIndex; i++) {
+            this.chars[i].style.color = this.colors[i];
+        }
+
+        if (this.isHovered) {
+            this.hueIndex++;
+        } else {
+            // increment startIndex, so that rainbow runs through rest of chars
+            this.startIndex = Math.min(this.startIndex + 1, this.charsLength);
+        }
+
+        // stop sparkling if there are no more chars to change
+        this.isSparkling = this.startIndex !== this.charsLength;
+
+        // keep sparkling
+        if (this.isSparkling) {
+            requestAnimationFrame(this.sparkle.bind(this));
+        }
+    };
+
+    // ---------- events ---------- //
+
+    /**
+     * Rainbowify link when the mouse hovers over it.
+     */
+    function onMouseover(event) {
+        let link = getTaggedElement(event.target, 'a');
+        if (!link || link === hoveredLink) {
+            return;
+        }
+
+        new SparkleShineLink(link);
     }
-}
 
-testStorage();
+    /**
+     * Checks if elem matches a tag
+     * @param element Element
+     * @param tag String - 'a', 'span'
+     **/
+    function getTaggedElement(element, tag) {
+        // walk up DOM, see if elem is <a>
+        while ( element.nodeType !== 9 ) {
+            if ( element.nodeName.toLowerCase() === tag ) {
+                return element;
+            }
+            element = element.parentNode;
+        }
+        return false;
+    }
 
+    // Add event listener for mouseover to trigger rainbowify on a link
+    document.addEventListener( 'mouseover', onMouseover, false );
 
-
+})(window, document);
