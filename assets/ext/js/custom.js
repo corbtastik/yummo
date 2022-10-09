47a4f2da67218b2e2cac20df8754428e748d93b9
corbs <corbtastik@gmail.com>
2022-10-09 11:15:55 -0500

    Complete custom js example that uses AJAX to retrieve JSON data from the server

--- assets/ext/js/custom.js
+++ assets/ext/js/custom.js
@@ -23,21 +23,67 @@
     YummoRequest.prototype.handleRequest = function() {
         if(this.readyState === XMLHttpRequest.DONE) {
             if(this.status === 200) {
-                let yummoInfo = JSON.parse(this.responseText);
-                console.log(JSON.stringify(yummoInfo));
-                let div = document.getElementById("yummo-info");
-                
+                createView(JSON.parse(this.responseText));
             } else {
                 console.log("Oh no that's a YummoRequest error!");
             }
         }
     }
 
+    function createView(yummoInfo) {
+        // Remove yummo-info element if it exists and create a new one below
+        if(document.getElementById("yummo-info")) {
+            document.getElementById("yummo-info").remove();
+        }
+
+        // Create an unordered list to render yummo-info into
+        const element = document.createElement("ul");
+        element.setAttribute("id", "yummo-info");
+        // Get the parent element
+        const parent = document.getElementById("yummo-button").parentElement;
+        // Append dynamic data to parent element
+        parent.append(element);
+
+        // Create textNodes for each yummo-info property
+        const retrievedAtNode = document.createTextNode("Retrieved at: " + new Date().toLocaleString());
+        const titleNode = document.createTextNode("Title: " + yummoInfo.title);
+        const tagNode = document.createTextNode("Tag: " + yummoInfo.tag_text);
+        const descriptionNode = document.createTextNode("Description: " + yummoInfo.description);
+        const versionNode = document.createTextNode("Version: " + yummoInfo.version);
+        const buildDateNode = document.createTextNode("Build Date: " + yummoInfo.build_date);
+
+        // Add retrieve time first
+        let listItem = document.createElement("li");
+        listItem.appendChild(retrievedAtNode);
+        document.getElementById("yummo-info").appendChild(listItem);
+
+        // Append each list item as a child of the unordered list
+        listItem = document.createElement("li");
+        listItem.appendChild(titleNode);
+        document.getElementById("yummo-info").appendChild(listItem);
+
+        listItem = document.createElement("li");
+        listItem.appendChild(tagNode);
+        document.getElementById("yummo-info").appendChild(listItem);
+
+        listItem = document.createElement("li");
+        listItem.appendChild(descriptionNode);
+        document.getElementById("yummo-info").appendChild(listItem);
+
+        listItem = document.createElement("li");
+        listItem.appendChild(versionNode);
+        document.getElementById("yummo-info").appendChild(listItem);
+
+        listItem = document.createElement("li");
+        listItem.appendChild(buildDateNode);
+        document.getElementById("yummo-info").appendChild(listItem);
+    }
+
     /**
      * Handle button click event
      */
     function onClick() {
-        new YummoRequest("http://localhost:4002/yummo.json")
+        new YummoRequest("/yummo.json")
             .get();
     }
 
